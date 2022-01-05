b230b1778017f04cb0757307322296e9f53e4337
MajickTek <kingtmw2@gmail.com>
2022-01-05 15:39:31 -0500

    Clean up char.php, more to come

--- views/char.php
+++ views/char.php
@@ -1,19 +1,19 @@
 		<div class="box pane">
 <?php
 	if (isset($_SESSION['char_id'])){
-		$sql = "SELECT `name`, `objectID`, `unapprovedName`, `lastZoneId`, `mapInstance`, `mapClone`, `x`, `y`, `z` FROM `characters` WHERE `objectID` = '" . $_SESSION['char_id'] . "'";
+		$sql = "SELECT * FROM `charinfo` WHERE `id` = '" . $_SESSION['char_id'] . "'";
 		$res = $mysql->query($sql);
 		if ($res->num_rows > 0){
 			$obj = $res->fetch_object();
-			$objid = $obj->objectID;
-			$uname = $obj->unapprovedName;
+			$objid = $obj->id;
+			//$uname = $obj->unapprovedName;
 			$name = $obj->name;
 ?>
-		<h1 style="margin: 0;"><?php echo $name . " "; if ($uname != "") echo "(" . $uname . ") "?></h1>
-		<h3>[ObjectID: <?php echo $objid; ?>]</h3>
+		<h1 style="margin: 0;"><?php echo $name . " "; //if ($uname != "") echo "(" . $uname . ") "?></h1>
+		<h3>ID: <?php echo $objid; ?>]</h3>
 		<br/>
-		<span>Zone: <?php echo $obj->lastZoneId; ?>, Instance: <?php echo $obj->mapInstance; ?>, Clone: <?php echo $obj->mapClone; ?>, Position: (<?php echo $obj->x . "|" . $obj->y . "|" . $obj->z; ?>)</span><br/>
-		<span style="font-size: 9pt; color: #B00;">This position is only updated on world change at the moment</span>
+		<!--<span>Zone: <?php echo $obj->lastZoneId; ?>, Instance: <?php echo $obj->mapInstance; ?>, Clone: <?php echo $obj->mapClone; ?>, Position: (<?php echo $obj->x . "|" . $obj->y . "|" . $obj->z; ?>)</span><br/>-->
+		<!--<span style="font-size: 9pt; color: #B00;">This position is only updated on world change at the moment</span>-->
 <?php
 		}
 	}else{
